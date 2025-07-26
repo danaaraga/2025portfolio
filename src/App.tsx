8e42ed6cb799f033daa097692f30e3cee770171c
danaaraga <danaraga101@gmail.com>
2025-07-26 12:18:54 +0700

    Menambahkan asset foto dan menyelesaikan project.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Certifications from './components/Certifications';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Certifications />
         <Projects />
         <Contact />
+        <Footer />
       </main>
     </div>
   );
